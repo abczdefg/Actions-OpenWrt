f2435c63582c6034ade82adedd6e81779de3334b
czd <304275574@qq.com>
2021-12-01 07:55:29 -0800

    use 21

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
